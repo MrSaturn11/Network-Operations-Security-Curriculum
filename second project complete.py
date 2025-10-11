df9c2a0347b009d26131a7415cf8ef11be423cb1
MrSaturn11 <122693130+MrSaturn11@users.noreply.github.com>
2025-10-11 13:50:32 +0100

    second project complete.py

--- second project complete.py
+++ second project complete.py
@@ -5,9 +5,29 @@ SEARCH_ENGINE_ID = '54babb708747d40ce'
 query = input('What do you want to search?: ')
 
 url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={API_KEY}&cx={SEARCH_ENGINE_ID}"
-
+#url is googles custom search API
 response = requests.get(url) #sends http request to the url above
-data = response.json() #converts response from json into python object (so now can access it using syntax)
+data = response.json() #converts response from javascript into python object (so now can access it using syntax)
 
-for i, item in enumerate(data.get("items")[:10], start=1): #looks for items in the main json directory which contains the title data
-    print(f"{i}. {item['title']}") #goes inside the items folder and extracts the titles inside that directory
+items = data.get("items")
+first = items[0]
+second = items[1]
+third = items[2]
+fourth = items[3]
+fifth = items[4]
+sixth = items[5]
+seventh =  items[6]
+eigth = items[7]
+ninth = items[8]
+tenth = items[9]
+print(first['title'])
+print(second['title'])
+print(third['title'])
+print(fourth['title'])
+print(fifth['title'])
+print(sixth['title'])
+print(seventh['title'])
+print(eigth['title'])
+print(ninth['title'])
+print(tenth['title'])
+
