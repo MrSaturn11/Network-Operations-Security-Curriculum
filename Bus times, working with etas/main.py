e3431998f8772682360fb0ff003da9d469461be9
MrSaturn11 <122693130+MrSaturn11@users.noreply.github.com>
2025-11-02 13:24:42 +0000

    Update main.py

--- Bus times, working with etas/main.py
+++ Bus times, working with etas/main.py
@@ -2,7 +2,7 @@ import time
 from tfwm_service import TfwmService 
 
 ATCO_CODE = "43000203903" #bus stop code 
-REFRESH_MS = 30_000
+REFRESH_SECONDS = 30
 
 def main():
     while True:
@@ -11,7 +11,8 @@ def main():
         for t in times:
             live = "LIVE" if not t.is_timetabled else "TIMETABLED" #show ETA times else show timetabled times
             print(f"{t.number:<4}{t.name:<25}{t.scheduled_time:8}{live}")
-        time.sleep(REFRESH_MS / 1000)
+        time.sleep(REFRESH_MS)
 if __name__ == "__main__":
     main()
+
 
