e1b157cee733f7eec21cb5e7343b976edae78d4a
MrSaturn11 <122693130+MrSaturn11@users.noreply.github.com>
2025-11-03 14:19:18 +0000

    Updated tfwm_service.py

--- Bus times, working with etas/tfwm_service.py
+++ Bus times, working with etas/tfwm_service.py
@@ -1,5 +1,6 @@
 import requests
 from bus_time import BusTime
+from datetime import datetime
 
 APP_ID = "a265ddae"
 APP_KEY = "7529d0b95200d8f98d16f755b80e9065"
@@ -23,9 +24,18 @@ class TfwmService:
         out = []
         for line, buses in data.get("departures", {}).items():
             for b in buses:
-                mins = b.get("best_departure_estimate_mins") # if it finds the best_departure_estimate_mins value then gives ETA
+                mins = b.get("best_departure_estimate_mins") # if it finds the best_departure_estimate_mins value then gives ETA based off that
+                if mins is None: #if best_departure_estimate_mins has no value then manually calc eta from timetabled time
+                    aimed = b.get("aimed_departure_time")
+                    if aimed: 
+                        today = datetime.now().date()
+                        aimed_dt = datetime.strptime(f"{today} {aimed}", "%Y-%m-%d %H:%M")
+                        diff = (aimed_dt - datetime.now()).total_seconds() / 60
+                        mins = max(int(diff),0)
                 direction = b.get("direction", "") 
-                time = f"{mins} mins" if mins is not None else b.get("aimed_departure_time", "N/A") #if mins is None show timetabled time
-                is_timetabled = mins is None
-                out.append(BusTime(b["line"], direction, time, is_timetabled))
+                    
+                time = f"{mins} mins" if mins is not None else b.get("aimed_departure_time", "N/A") #if live mins are available show them else show scheduled timetable or NA if missing
+                is_timetabled = mins is None #falg if  entry missing
+                out.append(BusTime(b["line"], direction, time, is_timetabled)) #bus timetable object using all ionfo 
+                
         return out
