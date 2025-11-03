70e00d63eabe060851f91f87766dea85fc865005
MrSaturn11 <122693130+MrSaturn11@users.noreply.github.com>
2025-11-03 14:18:39 +0000

    Updated main.py

--- Bus times, working with etas/main.py
+++ Bus times, working with etas/main.py
@@ -1,8 +1,6 @@
-import time
 from tfwm_service import TfwmService 
 
-ATCO_CODE = "43000203903" #bus stop code 
-REFRESH_SECONDS = 30.0
+ATCO_CODE = "43000203903" #unique bus stop code identifier  
 
 def main():
     while True:
@@ -11,9 +9,7 @@ def main():
         for t in times:
             live = "LIVE" if not t.is_timetabled else "TIMETABLED" #show ETA times else show timetabled times
             print(f"{t.number:<4}{t.name:<25}{t.scheduled_time:8}{live}")
-        time.sleep(REFRESH_MS)
+        
+        break #could loop every 30 seconds or so but dont want to reach my free limit too quickly
 if __name__ == "__main__":
     main()
-
-
-
